39877284eb6148b6143c05e15f316f7c6d7e7a0e
NabillahAnnisa <NabillahAnnisa@users.noreply.github.com>
2015-04-19 21:34:50 +0700

    Update Cetakan_JmlKonPosisiSarana.java

--- src/RuangKelas/Cetakan_JmlKonPosisiSarana.java
+++ src/RuangKelas/Cetakan_JmlKonPosisiSarana.java
@@ -1,5 +1,31 @@
 package RuangKelas;
 
 public class Cetakan_JmlKonPosisiSarana {
-   //ini bagian bella 
+   private int jml_steker;
+private String kondisi_steker;
+private String posisi_steker;
+
+private int jml_kabelLCD;
+private String kondisi_kabelLCD;
+private String posisi_kabelLCD;
+
+private int jml_lampu;
+private String kondisi_lampu;
+private String posisi_lampu;
+
+private int jml_KipasAngin;
+private String kondisi_KipasAngin;
+private String posisi_KipasAngin;
+
+private int jml_AC;
+private String kondisi_AC;
+private String posisi_AC;
+
+private String SSID;
+private String bandwidth;
+
+private int jml_CCTV;
+private String kondisi_CCTV;
+private String posisi_CCTV;
+
 }

462e1782d3e6364a074b39ec5aa29ce4462bcb6b
NabillahAnnisa <NabillahAnnisa@users.noreply.github.com>
2015-04-19 21:42:52 +0700

    Update Cetakan_KenyamananRK.java

--- src/RuangKelas/Cetakan_KenyamananRK.java
+++ src/RuangKelas/Cetakan_KenyamananRK.java
@@ -1,15 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package RuangKelas;
 
-/**
- *
- * @author acer
- */
 public class Cetakan_KenyamananRK {
     
+private string kebisingan;
+private string bau;
+private string kebocoran;
+private string kerusakan;
+private string keausan;
+
+    public string getKebisingan() {
+        return kebisingan;
+    }
+
+    public void setKebisingan(string kebisingan) {
+        this.kebisingan = kebisingan;
+    }
+
+    public string getBau() {
+        return bau;
+    }
+
+    public void setBau(string bau) {
+        this.bau = bau;
+    }
+
+    public string getKebocoran() {
+        return kebocoran;
+    }
+
+    public void setKebocoran(string kebocoran) {
+        this.kebocoran = kebocoran;
+    }
+
+    public string getKerusakan() {
+        return kerusakan;
+    }
+
+    public void setKerusakan(string kerusakan) {
+        this.kerusakan = kerusakan;
+    }
+
+    public string getKeausan() {
+        return keausan;
+    }
+
+    public void setKeausan(string keausan) {
+        this.keausan = keausan;
+    }
+
+
+
 }

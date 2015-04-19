e24d8fa06631bb30eb980e79d2e36ea3a457d6d3
NabillahAnnisa <NabillahAnnisa@users.noreply.github.com>
2015-04-19 21:39:07 +0700

    Update Cetakan_JmlKonPosisiSarana.java

--- src/RuangKelas/Cetakan_JmlKonPosisiSarana.java
+++ src/RuangKelas/Cetakan_JmlKonPosisiSarana.java
@@ -1,10 +1,7 @@
 package RuangKelas;
 
 public class Cetakan_JmlKonPosisiSarana {
-   package RuangKelas;
-
-public class Cetakan_JmlKonPosisiSarana {
-private int jml_steker;
+   private int jml_steker;
 private String kondisi_steker;
 private String posisi_steker;
 
@@ -31,6 +28,164 @@ private int jml_CCTV;
 private String kondisi_CCTV;
 private String posisi_CCTV;
 
-}
+    public int getJml_steker() {
+        return jml_steker;
+    }
 
+    public void setJml_steker(int jml_steker) {
+        this.jml_steker = jml_steker;
+    }
+
+    public String getKondisi_steker() {
+        return kondisi_steker;
+    }
+
+    public void setKondisi_steker(String kondisi_steker) {
+        this.kondisi_steker = kondisi_steker;
+    }
+
+    public String getPosisi_steker() {
+        return posisi_steker;
+    }
+
+    public void setPosisi_steker(String posisi_steker) {
+        this.posisi_steker = posisi_steker;
+    }
+
+    public int getJml_kabelLCD() {
+        return jml_kabelLCD;
+    }
+
+    public void setJml_kabelLCD(int jml_kabelLCD) {
+        this.jml_kabelLCD = jml_kabelLCD;
+    }
+
+    public String getKondisi_kabelLCD() {
+        return kondisi_kabelLCD;
+    }
+
+    public void setKondisi_kabelLCD(String kondisi_kabelLCD) {
+        this.kondisi_kabelLCD = kondisi_kabelLCD;
+    }
+
+    public String getPosisi_kabelLCD() {
+        return posisi_kabelLCD;
+    }
+
+    public void setPosisi_kabelLCD(String posisi_kabelLCD) {
+        this.posisi_kabelLCD = posisi_kabelLCD;
+    }
+
+    public int getJml_lampu() {
+        return jml_lampu;
+    }
+
+    public void setJml_lampu(int jml_lampu) {
+        this.jml_lampu = jml_lampu;
+    }
+
+    public String getKondisi_lampu() {
+        return kondisi_lampu;
+    }
+
+    public void setKondisi_lampu(String kondisi_lampu) {
+        this.kondisi_lampu = kondisi_lampu;
+    }
+
+    public String getPosisi_lampu() {
+        return posisi_lampu;
+    }
+
+    public void setPosisi_lampu(String posisi_lampu) {
+        this.posisi_lampu = posisi_lampu;
+    }
+
+    public int getJml_KipasAngin() {
+        return jml_KipasAngin;
+    }
+
+    public void setJml_KipasAngin(int jml_KipasAngin) {
+        this.jml_KipasAngin = jml_KipasAngin;
+    }
+
+    public String getKondisi_KipasAngin() {
+        return kondisi_KipasAngin;
+    }
+
+    public void setKondisi_KipasAngin(String kondisi_KipasAngin) {
+        this.kondisi_KipasAngin = kondisi_KipasAngin;
+    }
+
+    public String getPosisi_KipasAngin() {
+        return posisi_KipasAngin;
+    }
+
+    public void setPosisi_KipasAngin(String posisi_KipasAngin) {
+        this.posisi_KipasAngin = posisi_KipasAngin;
+    }
+
+    public int getJml_AC() {
+        return jml_AC;
+    }
+
+    public void setJml_AC(int jml_AC) {
+        this.jml_AC = jml_AC;
+    }
+
+    public String getKondisi_AC() {
+        return kondisi_AC;
+    }
+
+    public void setKondisi_AC(String kondisi_AC) {
+        this.kondisi_AC = kondisi_AC;
+    }
+
+    public String getPosisi_AC() {
+        return posisi_AC;
+    }
+
+    public void setPosisi_AC(String posisi_AC) {
+        this.posisi_AC = posisi_AC;
+    }
+
+    public String getSSID() {
+        return SSID;
+    }
+
+    public void setSSID(String SSID) {
+        this.SSID = SSID;
+    }
+
+    public String getBandwidth() {
+        return bandwidth;
+    }
+
+    public void setBandwidth(String bandwidth) {
+        this.bandwidth = bandwidth;
+    }
+
+    public int getJml_CCTV() {
+        return jml_CCTV;
+    }
+
+    public void setJml_CCTV(int jml_CCTV) {
+        this.jml_CCTV = jml_CCTV;
+    }
+
+    public String getKondisi_CCTV() {
+        return kondisi_CCTV;
+    }
+
+    public void setKondisi_CCTV(String kondisi_CCTV) {
+        this.kondisi_CCTV = kondisi_CCTV;
+    }
+
+    public String getPosisi_CCTV() {
+        return posisi_CCTV;
+    }
+
+    public void setPosisi_CCTV(String posisi_CCTV) {
+        this.posisi_CCTV = posisi_CCTV;
+    }
+ 
 }

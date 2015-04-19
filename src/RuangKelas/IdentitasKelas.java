ef60336908de24ae92067389ebc96fb8d929d004
NabillahAnnisa <NabillahAnnisa@users.noreply.github.com>
2015-04-19 21:46:12 +0700

    Update IdentitasKelas.java

--- src/RuangKelas/IdentitasKelas.java
+++ src/RuangKelas/IdentitasKelas.java
@@ -1,15 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package RuangKelas;
 
-/**
- *
- * @author acer
- */
+import java.util.Scanner;
+
 public class IdentitasKelas {
+    Cetakan_IdentitasKelas IDkelas = new Cetakan_IdentitasKelas();
     
+    public void insertdata_IDKELAS (){
+        Scanner ID = new Scanner (System.in);
+        
+         System.out.println("Nama Lengkap : ");
+       IDkelas.setNama(input.nextLine());
+        System.out.println("Alamat : ");
+       IDkelas.setLokasi(input.nextLine());
+        System.out.println("Fakultas/Jurusan : ");
+       IDkelas.setProdi_fakultas(input.nextLine());
+    }
+      
+    public void cetakdata_IDKELAS (){
+        System.out.println("**************************************");
+        System.out.println("Judul : "+IDkelas.getNama());
+        System.out.println("Penulis : "+IDkelas.getLokasi());
+        System.out.println("Jumlah Halaman : "+IDkelas.getProdi_fakultas()+"\n");
+        System.out.println("**************************************");
+        
+    }
 }

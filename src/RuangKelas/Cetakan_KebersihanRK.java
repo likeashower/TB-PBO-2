5e01ae3fe63834ae1643cc5000f0698e0f6bef52
NabillahAnnisa <NabillahAnnisa@users.noreply.github.com>
2015-04-19 21:41:16 +0700

    Update Cetakan_KebersihanRK.java

--- src/RuangKelas/Cetakan_KebersihanRK.java
+++ src/RuangKelas/Cetakan_KebersihanRK.java
@@ -1,15 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package RuangKelas;
 
-/**
- *
- * @author acer
- */
 public class Cetakan_KebersihanRK {
-    
+private String sirkulasi_udara;
+private int Nilai_Pencahayaan;
+private int kelembapan;
+private int suhu;
+
+    public String getSirkulasi_udara() {
+        return sirkulasi_udara;
+    }
+
+    public void setSirkulasi_udara(String sirkulasi_udara) {
+        this.sirkulasi_udara = sirkulasi_udara;
+    }
+
+    public int getNilai_Pencahayaan() {
+        return Nilai_Pencahayaan;
+    }
+
+    public void setNilai_Pencahayaan(int Nilai_Pencahayaan) {
+        this.Nilai_Pencahayaan = Nilai_Pencahayaan;
+    }
+
+    public int getKelembapan() {
+        return kelembapan;
+    }
+
+    public void setKelembapan(int kelembapan) {
+        this.kelembapan = kelembapan;
+    }
+
+    public int getSuhu() {
+        return suhu;
+    }
+
+    public void setSuhu(int suhu) {
+        this.suhu = suhu;
+    }
+ 
+
 }

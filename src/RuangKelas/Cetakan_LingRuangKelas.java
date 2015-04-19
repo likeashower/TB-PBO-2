7898b259c2afb756ee6c2cad1d00c464501e5457
NabillahAnnisa <NabillahAnnisa@users.noreply.github.com>
2015-04-19 21:44:09 +0700

    Update Cetakan_LingRuangKelas.java

--- src/RuangKelas/Cetakan_LingRuangKelas.java
+++ src/RuangKelas/Cetakan_LingRuangKelas.java
@@ -1,15 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package RuangKelas;
 
-/**
- *
- * @author acer
- */
+
 public class Cetakan_LingRuangKelas {
     
+private String kondisi_lantai;
+private String kondisi_dinding;
+private String kondisi_atap;
+private String kondisi_pintu;
+private String kondisi_jendela;
+
+    public String getKondisi_lantai() {
+        return kondisi_lantai;
+    }
+
+    public void setKondisi_lantai(String kondisi_lantai) {
+        this.kondisi_lantai = kondisi_lantai;
+    }
+
+    public String getKondisi_dinding() {
+        return kondisi_dinding;
+    }
+
+    public void setKondisi_dinding(String kondisi_dinding) {
+        this.kondisi_dinding = kondisi_dinding;
+    }
+
+    public String getKondisi_atap() {
+        return kondisi_atap;
+    }
+
+    public void setKondisi_atap(String kondisi_atap) {
+        this.kondisi_atap = kondisi_atap;
+    }
+
+    public String getKondisi_pintu() {
+        return kondisi_pintu;
+    }
+
+    public void setKondisi_pintu(String kondisi_pintu) {
+        this.kondisi_pintu = kondisi_pintu;
+    }
+
+    public String getKondisi_jendela() {
+        return kondisi_jendela;
+    }
+
+    public void setKondisi_jendela(String kondisi_jendela) {
+        this.kondisi_jendela = kondisi_jendela;
+    }
+
+
 }

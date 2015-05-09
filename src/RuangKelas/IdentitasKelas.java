632001fe48de8e7cbad27d223f0bcd62dd4accc5
likeashower <ulfie.khalida@gmail.com>
2015-05-09 18:10:49 +0800

    Update : Saturday, 2015/05/09 by Ulfie Khalida Inheritance

--- src/RuangKelas/IdentitasKelas.java
+++ src/RuangKelas/IdentitasKelas.java
@@ -1,15 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package RuangKelas;
+/*UPDATES
+Sunday, 2015/04/19 Created by Ulfie.K.
+Sunday, 2015/04/19 Updated by Nabillah.A. (Method input & output)
+*/
+import java.util.Scanner;
 
-/**
- *
- * @author acer
- */
-public class IdentitasKelas {
+public class IdentitasKelas extends SuperClass{
+    Cetakan_IdentitasKelas IDkelas = new Cetakan_IdentitasKelas();
     
+    void insertdata_IDKELAS (){
+         
+       System.out.println("Nama Ruangan : ");
+       IDkelas.setNama(input.nextLine());
+        System.out.println("Lokasi: ");
+       IDkelas.setLokasi(input.nextLine());
+        System.out.println("Fakultas/Jurusan : ");
+       IDkelas.setProdi_fakultas(input.nextLine());
+    }
+      
+    public void cetakdata_IDKELAS (){
+        System.out.println("Nama Ruangan : "+IDkelas.getNama());
+        System.out.println("Lokasi : "+IDkelas.getLokasi());
+        System.out.println("Fakultas/Jurusan: "+IDkelas.getProdi_fakultas()+"\n");
+    }
 }
